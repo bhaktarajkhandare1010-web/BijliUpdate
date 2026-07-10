c35f93d463e8f1dc057bee6c8909b478bed7de45
root <root@localhost.localdomain>
2026-07-10 13:37:36 +0530

    Fix analyzer issues

--- services/auth_service.dart firestore_service.dart notification_service.dart
+++
@@ -1,9 +0,0 @@
-class FirestoreService {
-  Future<void> addComplaint() async {
-    print("Complaint Saved");
-  }
-}class FirestoreService {
-  Future<void> addComplaint() async {
-    print("Complaint Saved");
-  }
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:bijli_update/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const BijliUpdateApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
