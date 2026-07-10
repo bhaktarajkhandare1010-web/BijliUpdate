df26b23831c8204e5f7a0525420ae4b22df21259
root <root@localhost.localdomain>
2026-07-10 13:03:43 +0530

    Add Internet permission and update NDK version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 android {
     namespace = "com.example.bijli_update"
     compileSdk = 34
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
